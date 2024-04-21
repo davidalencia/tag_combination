597ea9fa78e740c61c285d72956fd36283987e2c
David Valencia <davidalencia@gmail.com>
2024-04-21 03:20:23 -0600

    railway

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"os"
 	routes "tag_combination_api/routes"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+	} else {
+		port = ":" + port
+	}
+
+	return port
+}
 
 func main() {
 
 
 	// notion.UpdateDatabase("62d01a2d65284e2f847809abfe3b88da")
 
+	
+
   app := fiber.New()
 
   routes.Register(app)
 
-  app.Listen(":3000")
+  app.Listen(getPort())
 }
